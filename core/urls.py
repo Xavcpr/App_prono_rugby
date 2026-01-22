88abe9a562648abe7a9db32544096c09a0710850
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-22 17:07:07 +0100

    import et rugby_admin_site.urls correction

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from core.admin import rugby_admin_site
+from django.contrib import admin
+
+# from core.admin import rugby_admin_site
 
 urlpatterns = [
-    path("admin/", rugby_admin_site.urls),
+    # path("admin/", rugby_admin_site.urls),
+    path('admin/', admin.site.urls),
     path('pronos/', views.pronos_view, name='pronostics'),
     path('logout/', views.logout_view, name='logout'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
