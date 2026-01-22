f50e14b9111b939aab5eaba2eac9a1706a94363d
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-22 20:22:18 +0100

    views, admin, pronos  correction

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
+from .views import pronos_view, logout_view
 
 # from core.admin import rugby_admin_site
 
 urlpatterns = [
     # path("admin/", rugby_admin_site.urls),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('pronos/', views.pronos_view, name='pronostics'),
     path('logout/', views.logout_view, name='logout'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
