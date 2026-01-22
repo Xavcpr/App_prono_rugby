ee4f57eb44adbc99cc9f2e0890365c99faf79b59
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-22 19:52:30 +0100

    views, admin, pronos  correction

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('accounts/', include('django.contrib.auth.urls')),  # 👈 AJOUT
+#     path('', include('core.urls')),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
+from core.admin import rugby_admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # 👈 AJOUT
-    path('', include('core.urls')),
+    path("admin/", admin.site.urls),  # admin classique
+    path("rugby-admin/", rugby_admin_site.urls),  # admin custom
+    path("", include("core.urls")),
 ]
