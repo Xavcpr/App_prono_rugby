4b7f2ad64a6efa65c61297f796bee35cb993d308
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-22 20:38:49 +0100

    views, admin, pronos  correction

--- core/urls.py
+++ core/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path("pronos/", pronos_view, name="pronostics"),
     path("logout/", logout_view, name="logout"),
 ]
+
+from django.http import HttpResponse
+
+def test_url(request):
+    return HttpResponse("OK PRONOS")
+
+urlpatterns = [
+    path("pronos/", pronos_view, name="pronostics"),
+    path("test-pronos/", test_url),
+]
