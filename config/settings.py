31d9db775da0876bf594ab292b0b865b7698eb26
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-25 06:48:48 +0100

    passage en zone France

--- config/settings.py
+++ config/settings.py
@@ -110,11 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Paris"
+USE_TZ = True
 
 USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
