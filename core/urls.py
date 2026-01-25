d1da003d9cb1b82696cef8b2dc4fd2a60f130a60
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-25 15:24:41 +0100

    moving pronos.html to pronos/pronos.html

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import pronos_view, logout_view, settings_view, competition_ranking_view
-from backend.core import views
+from .views import pronos_view, logout_view, settings_view, competition_ranking_view, classement_prediction
 
 urlpatterns = [
     path("pronos/", pronos_view, name="pronostics"),
     path("logout/", logout_view, name="logout"),
     path('settings/', settings_view, name='settings'),
     path("classement_par_competition/", competition_ranking_view, name="classement_par_competition"),
-    path("pronos/classement/", views.classement_prediction, name="classement_prediction"),
+    path("pronos/classement/", classement_prediction, name="classement_prediction"),
 ]
 
 
