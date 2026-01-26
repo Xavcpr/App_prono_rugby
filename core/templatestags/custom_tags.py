b37480885571cb35e9fb5830fa11c253fea83cd5
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-26 20:05:37 +0100

    bug fix

--- core/templatestags/custom_tags.py
+++ core/templatestags/custom_tags.py
@@ -1,7 +1,7 @@
-# core/templatetags/custom_tags.py
-from django import template
-register = template.Library()
+# # core/templatetags/custom_tags.py
+# from django import template
+# register = template.Library()
 
-@register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
+# @register.filter
+# def get_item(dictionary, key):
+#     return dictionary.get(key)
