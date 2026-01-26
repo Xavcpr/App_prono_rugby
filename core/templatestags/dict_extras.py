630834c0230b3e972d05e774bcae60e7b6586520
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-01-26 20:12:17 +0100

    bug fix

--- core/templatestags/dict_extras.py
+++ core/templatestags/dict_extras.py
@@ -4,6 +4,4 @@ register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    if dictionary:
-        return dictionary.get(key)
-    return None
+    return dictionary.get(key)
