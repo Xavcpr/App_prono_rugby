9735ba66b921fe5f861343b074d95112b0c651d4
Xavier Fabiani <xavier.fabiani@hotmail.fr>
2026-07-10 23:44:10 +0200

    signal round email ne plante plus si reseau indisponible

--- core/signals.py
+++ core/signals.py
@@ -1,10 +1,17 @@
+import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Round
 from .services.email_service import notify_new_round
 
 
+logger = logging.getLogger(__name__)
+
+
 @receiver(post_save, sender=Round)
 def round_created_notification(sender, instance, created, **kwargs):
     if created:
-        notify_new_round(instance)
+        try:
+            notify_new_round(instance)
+        except Exception:
+            logger.warning("Failed to send round notification email", exc_info=True)
